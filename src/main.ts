352ff56ade0913c154af9c4a17b8fa24c078334d
Werner <wernertraveleraus@gmail.com>
2025-11-28 20:37:30 -0300

    firebase config

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,14 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 
-    //  >>>>>>      REEMPLAZAR CONTENIDO DE firebaseConfig CON TU PROPIO FIREBASE   <<<<<<<
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyAyi8zxqje3IKoAEeEeXNARiQmlFuWna-E",
+    authDomain: "tienda-juguetes.firebaseapp.com",
+    projectId: "tienda-juguetes",
+    storageBucket: "tienda-juguetes.firebasestorage.app",
+    messagingSenderId: "481781740553",
+    appId: "1:481781740553:web:01ea86a671e146a0a9f4d8",
+    measurementId: "G-N0YDLQ4QC9"
 };
 
 bootstrapApplication(App, {
